9ed7615f9babcff1f555f05d503f02fbf93210f7
ramizapata04-web <ramizapata04@gmail.com>
2025-11-15 19:12:01 +0000

    guardar cambios

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,29 @@
 import hashlib
 
 def crack_sha1_hash(hash, use_salts = False):
-    return True
+    with open('top-10000-passwords.txt', 'r') as f:
+        passwords = [line.strip() for line in f]
+    if use_salts:
+        with open('known-salts.txt', 'r') as f:
+            salts = [line.strip() for line in f]
+    else:
+        salts = [""]
+
+    for password in passwords:
+        for salt in salts:
+            if use_salts:
+                combinations = [
+                    salt + password,
+                    password + salt,
+                    salt + password + salt  # También probar con sal en ambos lados
+                ]
+            else:
+                combinations = [password]
+            
+            for combo in combinations:
+                hash_obj = hashlib.sha1(combo.encode())
+                hashed_password = hash_obj.hexdigest()
+                if hashed_password == hash:
+                    return password
+                    
+    return "PASSWORD NOT IN DATABASE"
